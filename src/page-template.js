a73d0f71ac3a2f1033dd0ac7348efbf89e7fe33f
Ernesto Valecillos <evalecillos@gmail.com>
2021-10-16 21:14:04 -0500

    fixed some spacing, deleted dead lines

--- src/page-template.js
+++ src/page-template.js
@@ -2,14 +2,14 @@ const generateManagerCard = function (manager) {
     return `
     <div class="card employee-card col-3">
     <div class="card-header">
-        <h2 class="card-title">${manager.getName()}</h2>
-        <h3 class="card-title">${manager.getRole()}</h3>
+        <h2 class="card-title"> ${manager.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i> ${manager.getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
-            <li class="list-group-item">ID:${manager.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-            <li class="list-group-item">Office #:${manager.getOfficeNumber()}</li>
+            <li class="list-group-item">ID: ${manager.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}"> ${manager.getEmail()}</a></li>
+            <li class="list-group-item">Office #: ${manager.getOfficeNumber()}</li>
         </ul>
     </div>
 </div>
@@ -20,14 +20,14 @@ const generateEngineerCard = function (engineer) {
     return `
     <div class="card employee-card col-3">
     <div class="card-header">
-        <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title">${engineer.getRole()}</h3>
+        <h2 class="card-title"> ${engineer.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i> ${engineer.getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
-            <li class="list-group-item">ID:${engineer.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target='_blank'>${engineer.getGithub()}</a></li>
+            <li class="list-group-item">ID: ${engineer.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}"> ${engineer.getEmail()}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target='_blank'> ${engineer.getGithub()}</a></li>
         </ul>
     </div>
 </div>
@@ -38,14 +38,14 @@ const generateInternCard = function (intern) {
     return `
     <div class="card employee-card col-3">
     <div class="card-header">
-        <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title">${intern.getRole()}</h3>
+        <h2 class="card-title"> ${intern.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i> ${intern.getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
-            <li class="list-group-item">ID:${intern.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-            <li class="list-group-item">School:${intern.getSchool()}</li>
+            <li class="list-group-item">ID: ${intern.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}"> ${intern.getEmail()}</a></li>
+            <li class="list-group-item">School: ${intern.getSchool()}</li>
         </ul>
     </div>
 </div>
@@ -99,7 +99,7 @@ const generateHtml = function (employeeCards) {
     <title>Staff Profiles</title>
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style.css" />
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
 <body>
     <div class="container-fluid">
